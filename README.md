ddf359718cc74fe4edb1bf59f8cb933df1be92a0
Alexey Orlenko <alex@aqrln.net>
2024-05-02 01:20:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# hello-stm32
+
+A simple program written for the STM32F401CEU6 microcontroller.
+
+The project structure was generated using [`cargo-embassy`](https://github.com/AdinAck/cargo-embassy).
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+#![no_std]
+#![no_main]
+
+mod fmt;
+
+#[cfg(not(feature = "defmt"))]
+use panic_halt as _;
+#[cfg(feature = "defmt")]
+use {defmt_rtt as _, panic_probe as _};
+
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_time::{Duration, Timer};
+use fmt::info;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+
+    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+    let btn = Input::new(p.PA0, Pull::Up);
+
+    const SEQ_TIME_MS: usize = 5000;
+    const TICK_TIME_MS: usize = 100;
+    const TICKS: usize = SEQ_TIME_MS / TICK_TIME_MS;
+    const MIN_IGNORED_TICKS_AFTER_RECORDING: usize = 3;
+
+    let mut levels = [Level::High; TICKS];
+    let mut tick = 0;
+
+    let wait = || Timer::after(Duration::from_millis(TICK_TIME_MS as u64));
+
+    loop {
+        info!("btn level: {}", btn.get_level());
+
+        led.set_level(levels[tick]);
+        tick += 1;
+        if tick >= levels.len() {
+            tick = 0;
+        }
+
+        if btn.is_high() {
+            wait().await;
+            continue;
+        }
+
+        for (i, saved_level) in levels.iter_mut().enumerate() {
+            info!("recording {}/{}", i, TICKS);
+            let btn_level = btn.get_level();
+            *saved_level = btn_level;
+            led.set_level(btn_level);
+            wait().await;
+        }
+
+        for _ in 0..MIN_IGNORED_TICKS_AFTER_RECORDING {
+            wait().await;
+        }
+
+        // prevent accidental restart of recording: if button is still pressed, wait until it's released
+        while btn.is_low() {
+            wait().await;
+        }
+
+        tick = 0;
+        wait().await;
+    }
+}
